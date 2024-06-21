0d73455046146b88f59723a38fe8c8b10f466f2d
Emilyy <emilyymeow@proton.me>
2024-06-21 11:29:07 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+        var repoName = "change in config.js"; 
+        var repoDescription = "open config.js with your text editor of choice."; 
+
+        document.querySelector('.repoName').innerText = repoName;
+        document.querySelector('.repodesc').innerText = repoDescription;
